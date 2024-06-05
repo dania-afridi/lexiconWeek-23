8d1fea272e7aba14bd3f66335f28abab006f64b6
dania-afridi <dania.awais33@gmail.com>
2024-06-05 09:21:46 +0200

    Add project files.

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,22 @@
+﻿using Assignment_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment_01.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult CheckFever()
+        {
+
+            double? Temperature = double.TryParse(Request.Query["Temperature"], out var temperatureValue)
+                ? temperatureValue
+                : (double?)null;
+
+            ViewBag.FeverMessage = FeverChecker.EvaluateFever(Temperature);
+            ViewBag.HypothermiaMessage = FeverChecker.EvaluateHypothermia(Temperature);
+            return View(Temperature);
+        }
+
+    }
+}
---
+++ Models/FeverChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_01.Models
+{
+    public static class FeverChecker
+    {
+        public static string EvaluateFever(double? Temperature)
+        {
+            if (Temperature.HasValue)
+            {
+                bool hasFever = Temperature.Value >= 37.5;
+                return hasFever ? "You have a fever." : "No fever detected.";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public static string EvaluateHypothermia(double? Temperature)
+        {
+            if (Temperature.HasValue)
+            {
+                bool hasHypothermia = Temperature.Value <= 35;
+                return hasHypothermia ? "You have hypothermia risk." : "";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
